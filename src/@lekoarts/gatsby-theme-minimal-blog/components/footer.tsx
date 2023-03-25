9f9cf01c69699f9f2e3ce4d835f5cb10f39b2a1f
admin <admin@Macintosh.local>
2023-03-25 21:22:41 +0800

    initial commit

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { Footer } from "@lekoarts/gatsby-theme-minimal-blog/src/components/footer"
+import useSiteMetadata from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-site-metadata"
+import { jsx, Link } from "theme-ui"
+
+const MyFooter = () => {
+  const { siteTitle } = useSiteMetadata()
+
+  return (
+    <footer
+      sx={{
+        boxSizing: `border-box`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        mt: [6],
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+        flexDirection: [`column`, `column`, `row`],
+        variant: `dividers.top`,
+      }}
+    >
+      <div>
+        &copy; {new Date().getFullYear()} by {siteTitle}. All rights reserved.
+      </div>
+    </footer>
+  )
+}
+
+export default MyFooter
