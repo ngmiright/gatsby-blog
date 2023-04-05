b3fe1b3df2117c5730bc82292a7be24a11e2eeaf
ngmiright <>
2023-04-05 17:13:30 +0800

    add starter post-footer component

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post-footer.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as React from "react"
+import type { MBPostProps } from "@lekoarts/gatsby-theme-minimal-blog/src/components/post"
+
+const PostFooter = ({ post }: MBPostProps) => {
+    return (
+        <div style={{ padding: '20vh' }}>
+        </div>
+    )
+}
+
+export default PostFooter
