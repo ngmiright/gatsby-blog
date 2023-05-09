96df54389376fb53ae7adfd9bc4c6ef217a33565
ngmiright <>
2023-05-09 20:46:55 +0800

    fix blockquote color in dark mode for smaller screens

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,9 +5,15 @@ lincolnTheme.baseFontSize = "16px"
 // to use color from gastby-plugin-theme-ui instead of lincolnTheme
 lincolnTheme.headerColor = ""
 lincolnTheme.bodyColor = ""
+
 lincolnTheme.overrideThemeStyles = ({ adjustFontSizeTo, scale, rhythm }, options) => ({
     a: {textShadow: "none", backgroundImage: "none"},
-    blockquote: {color: ""},
+    blockquote: { color: "inherit" },
+    '@media only screen and (max-width: 480px)': {
+        blockquote: {
+            color: "inherit", // to fix blockquote color in dark mode for smaller screens
+        },
+    },
 })
 
 const typography = new Typography({
